955803e704065191bad37c135c680814209e244f
谭翮舜 <58305222+hepsontam@users.noreply.github.com>
2021-05-17 02:14:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,12 +14,15 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 注释掉原luci
-sed -i 'luci/i\#' feeds.conf.default
+#sed -i 'luci/i\#' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #Mi AC2100使用
-sed -i '$a src-git luci https://github.com/openwrt/luci;openwrt-18.06' feeds.conf.default
-sed -i '$a src-git small https://github.com/hepsontam/small' feeds.conf.default
-sed -i '$a src-git openwrtpackages https://github.com/garypang13/openwrt-packages' feeds.conf.default
+echo 'src-git packages https://github.com/coolsnowwolf/packages' > feeds.conf.default
+echo 'src-git luci https://github.com/openwrt/luci;openwrt-18.06' >> feeds.conf.default
+echo 'src-git routing https://git.openwrt.org/feed/routing.git >> feeds.conf.default
+echo 'src-git telephony https://git.openwrt.org/feed/telephony.git >> feeds.conf.default
+echo 'src-git small https://github.com/hepsontam/small' >> feeds.conf.default
+echo 'src-git openwrtpackages https://github.com/garypang13/openwrt-packages' >> feeds.conf.default
